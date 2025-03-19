803eb197bd180624b33b314b9e5c6593e7fe6649
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-03-18 23:28:55 -0300

    Add files via upload

---
+++ ControleComissao/ControleComissao.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleComissao
+{
+    class ControleComissao
+    {
+        private string nome;
+        private double salarioBase;
+        private double comissao;
+        private double valorVendas;
+
+        // setNome
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        // getNome
+        public string getNome()
+        {
+            return nome;
+        }
+
+        // setComissao
+        public void setComissao(double comissao)
+        {
+            this.comissao = comissao;
+        }
+
+        // getComissao
+        public double getComissao()
+        {
+            return comissao;
+        }
+
+        // setSalarioBase
+        public void setSalarioBase(double salarioBase)
+        {
+            this.salarioBase = salarioBase;
+        }
+
+        // getSalarioBase
+        public double getSalarioBase()
+        {
+            return salarioBase;
+        }
+
+        // setvalorVendas
+        public void setValorVendas(double valorVendas)
+        {
+            this.valorVendas = valorVendas;
+        }
+
+        // getValorVendas
+        public double getValorVendas()
+        {
+            return valorVendas;
+        }
+
+        public double calcularComissao(double valorVendido)
+        {
+            comissao =  valorVendido * 0.03;
+            Console.WriteLine("Sua comissão é: " + comissao);
+            return valorVendido;
+        }
+
+    }
+}
---
+++ ControleComissao/Principal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ControleComissao
+{
+    class Principal
+    {
+        static void Main(string[] args)
+        {
+            ControleComissao cc1 = new ControleComissao();
+
+            Console.WriteLine("Digite seu nome: ");
+            cc1.setNome(Console.ReadLine());
+            Console.WriteLine("Digite seu salário base: ");
+            cc1.setSalarioBase(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite o valor das vendas: ");
+            
+
+
+            // Mostrar usuário
+
+            Console.WriteLine("Seu nome é : " + cc1.getNome());
+            Console.WriteLine("Seu seu salário é : " + cc1.getSalarioBase());
+            Console.WriteLine("O valor total das vendas é : " + cc1.getValorVendas());
+            cc1.calcularComissao(1500);
+        }
+    }
+}
